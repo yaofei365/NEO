9c7ff62e787ea34967539578517184c660ec0fbe
kinbei <kinbei@qq.com>
2019-08-13 09:02:44 +0800

    update

--- 3/project/client.bat
+++ 3/project/client.bat
@@ -3,5 +3,5 @@
 @SET CURRENT_DIR=%~dp0
 
 IF NOT EXIST "%CURRENT_DIR%client.lua" ECHO 需要按要求实现 client.lua 并放到 %CURRENT_DIR% 目录下 && pause && exit
-"%CURRENT_DIR%lua5.1.exe" client.lua server.config
+"%CURRENT_DIR%lua5.1.exe" client.lua client.config
 PAUSE
