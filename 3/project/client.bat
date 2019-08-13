2767dff78c164a7e813a8c17a7a18ffc3e7e6726
kinbei <kinbei@qq.com>
2019-08-13 08:44:56 +0800

    update

---
+++ 3/project/client.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+@SETLOCAL EnableDelayedExpansion
+@SET CURRENT_DIR=%~dp0
+
+IF NOT EXIST "%CURRENT_DIR%client.lua" ECHO 需要按要求实现 client.lua 并放到 %CURRENT_DIR% 目录下 && pause && exit
+"%CURRENT_DIR%lua5.1.exe" client.lua server.config
+PAUSE
--- 3/project/server.bat
+++ 3/project/server.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 @SETLOCAL EnableDelayedExpansion
 @SET CURRENT_DIR=%~dp0
+
+IF NOT EXIST "%CURRENT_DIR%server.lua" ECHO 需要按要求实现 server.lua 并放到 %CURRENT_DIR% 目录下 && pause && exit
 "%CURRENT_DIR%lua5.1.exe" server.lua server.config
 PAUSE
